927840c32bab87c83f187e343debd3d553c0613d
Gabriel Borba <gborba@id.uff.br>
2020-10-14 18:04:27 -0300

    wiget que mostra um resumo do carrinho/pedido

---
+++ lib/telas/revisar_pedido/components/panel_resume_carrinho.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'Carrinho.dart';
+
+class PanelResumeCarrinho extends StatefulWidget {
+  @override
+  _PanelResumeCarrinhoState createState() => _PanelResumeCarrinhoState();
+}
+
+class _PanelResumeCarrinhoState extends State<PanelResumeCarrinho> {
+  Carrinho carrinho;
+
+  @override
+  void initState() {
+    // REVIEW - will be fetched from database or received on constructor
+    carrinho = carrinhoTEMP;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 25),
+      child: Table(
+        columnWidths: {0: FlexColumnWidth(0.4), 1: FlexColumnWidth()},
+        defaultVerticalAlignment: TableCellVerticalAlignment.middle,
+        children: [
+          TableRow(
+            children: [
+              Container(
+                alignment: Alignment.centerLeft,
+                child: Text('Quantidade:'),
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 5),
+                alignment: Alignment.centerRight,
+                child: Text('${carrinho.length} itens'),
+              ),
+            ],
+          ),
+          TableRow(
+            children: [
+              Container(
+                alignment: Alignment.centerLeft,
+                child: Text('Subtotal:'),
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 5),
+                alignment: Alignment.centerRight,
+                child: Text(
+                  '${Carrinho.moeda} ${carrinho.subtotal.toStringAsFixed(2)}',
+                  maxLines: 1,
+                  style: Theme.of(context).textTheme.headline6,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
