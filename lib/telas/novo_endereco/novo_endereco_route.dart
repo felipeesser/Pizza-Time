43d9bd6c07070de189516c1e15b119c5fa6b19d2
Gabriel Borba <50279580+gborba-uff-cc@users.noreply.github.com>
2020-10-15 12:24:12 -0300

    implementação da rota novoEndereço. Esta rota é acessada através de um widget da rota finalizarPedido

---
+++ lib/telas/novo_endereco/novo_endereco_route.dart
@@ -0,0 +1,53 @@
+// NOTE - essa rota foi desenvolvida para ser chamada a partir da rota finalizarPedido.
+
+import 'package:flutter/material.dart';
+import 'components/form_novo_endereco.dart';
+
+class NovoEnderecoRoute extends StatefulWidget {
+  static const routeName = '/novo_endereco';
+  @override
+  _NovoEnderecoRouteState createState() => _NovoEnderecoRouteState();
+}
+
+class _NovoEnderecoRouteState extends State<NovoEnderecoRoute> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Novo endereço'),
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 5),
+          child: Column(
+            children: [
+              FormNovoEndereco(),
+              Spacer(),
+              ButtonBar(
+                alignment: MainAxisAlignment.start,
+                children: [
+                  RaisedButton(
+                      child: Text('salvar'.toUpperCase()),
+                      onPressed: () {
+                        _salvarEndereco();
+                      }),
+                  FlatButton(
+                    child: Text('cancelar'.toUpperCase()),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  _salvarEndereco() {
+    // TODO - salvar o endereço no banco de dados;
+    return;
+  }
+}
