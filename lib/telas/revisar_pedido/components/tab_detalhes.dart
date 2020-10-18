d1754a9d62dc76a60b84c9e8407d1b1d7c970c2a
Gabriel Borba <50279580+gborba-uff-cc@users.noreply.github.com>
2020-10-18 00:33:38 -0300

    abas para visualizar/revisar o pedido na visão do restaurante

---
+++ lib/telas/revisar_pedido/components/tab_detalhes.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'panel_resume_pedido_restaurante.dart';
+
+class TabDetalhes extends StatelessWidget {
+  static final tabName = 'Detalhes';
+
+  @override
+  Widget build(BuildContext context) {
+    return PanelResumePedidoRestaurante();
+  }
+}
---
+++ lib/telas/revisar_pedido/components/tab_produtos.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'listview_pedido_restaurante.dart';
+
+class TabProdutos extends StatelessWidget {
+  static final tabName = 'Produtos';
+
+  @override
+  Widget build(BuildContext context) {
+    return ListViewPedidoRestaurante();
+  }
+}
