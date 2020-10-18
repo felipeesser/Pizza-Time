e8c8fcb9ae4d8fa7e32fb0c60350fa37b234272d
Gabriel Borba <50279580+gborba-uff-cc@users.noreply.github.com>
2020-10-17 21:13:46 -0300

    rota onde o restaurante visualizará um pedido recebido,

---
+++ lib/telas/revisar_pedido/revisar_pedido_restaurante_route.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'components/listview_carrinho.dart';
+import 'components/panel_resume_carrinho.dart';
+import '../finalizar_pedido/finalizar_pedido_route.dart';
+
+class RevisarPedidoRoute extends StatelessWidget {
+  static final String routeName = '/revisar_pedido_restaurante';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(routeName),
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(vertical: 5),
+          child: Column(
+            children: [
+              Expanded(
+                child: ListViewCarrinho(),
+              ),
+              Divider(
+                thickness: 1.5,
+              ),
+              PanelResumeCarrinho(),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: RaisedButton(
+                  child: Text('finalizar pedido'.toUpperCase()),
+                  onPressed: () {
+                    _apresentarFinalizarPedido(context);
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  _apresentarFinalizarPedido(BuildContext context) async {
+    final res = await Navigator.pushNamed(
+      context,
+      FinalizarPedidoRoute.routeName,
+    );
+    if (res is Map<String, dynamic> && res['pedido_realizado']) {
+      Navigator.pop(context);
+    }
+  }
+}
