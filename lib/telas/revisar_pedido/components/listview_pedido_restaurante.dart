55c81b98e9b92bbeb4564051491204324ec12ae9
Gabriel Borba <50279580+gborba-uff-cc@users.noreply.github.com>
2020-10-20 17:14:45 -0300

    mantendo simplificado. KISS, keep it simple sir.

--- lib/telas/revisar_pedido/components/listview_pedido_restaurante.dart
+++ lib/telas/revisar_pedido/components/listview_pedido_restaurante.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'listitem_pedido_restaurante.dart';
+// import 'listitem_pedido_restaurante.dart';
 // TODO - remover as referencias ao carrinho assim que discutirmos como será o backend.
 import 'Carrinho.dart';
 
@@ -25,9 +25,34 @@ class _ListViewPedidoRestauranteState extends State<ListViewPedidoRestaurante> {
     return ListView.separated(
       itemCount: carrinho.length,
       itemBuilder: (context, index) {
-        return ListItemPedidoRestaurante(item: carrinho.itensPedido[index]);
+        // return ListItemPedidoRestaurante(item: carrinho.itensPedido[index]);
+        return _buildListItem(carrinho.itensPedido[index]);
       },
       separatorBuilder: (context, index) => Divider(),
+    );
+  }
+
+  /// Constrói a representação visual do item da lista de pedido do cliente.
+  _buildListItem(ItemPedido item) {
+    return ListTile(
+      dense: true,
+      title: Text(
+        item.nome,
+        maxLines: 1,
+        overflow: TextOverflow.ellipsis,
+      ),
+      leading: Icon(Icons.image),
+      subtitle: Text(
+        'Valor unitário: ${Carrinho.moeda} ${item.valor.toStringAsFixed(2).replaceAll('.', ',')}'
+        '\n'
+        'Quantidade: ${item.quantidade}',
+        maxLines: 2,
+      ),
+      trailing: Text(
+        '${Carrinho.moeda} ${item.subtotal.toStringAsFixed(2)}',
+        textAlign: TextAlign.center,
+      ),
+      onTap: () {},
     );
   }
 }
--- lib/telas/revisar_pedido/components/listview_pedido_usuario.dart
+++ lib/telas/revisar_pedido/components/listview_pedido_usuario.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'listitem_pedido_usuario.dart';
+// import 'listitem_pedido_usuario.dart';
+import 'dialog_edit_item.dart';
 // TODO - remover as referencias ao carrinho assim que discutirmos como será o backend.
 import 'Carrinho.dart';
 
@@ -30,9 +31,47 @@ class _ListViewPedidoUsuarioState extends State<ListViewPedidoUsuario> {
     return ListView.separated(
       itemCount: carrinho.length,
       itemBuilder: (context, index) {
-        return ListItemPedidoUsuario(item: carrinho.itensPedido[index]);
+        // return ListItemPedidoUsuario(item: carrinho.itensPedido[index]);
+        return _buildListItem(carrinho.itensPedido[index]);
       },
       separatorBuilder: (context, index) => Divider(),
+    );
+  }
+
+  /// Constrói a representação visual do item da lista de pedido do cliente.
+  _buildListItem(ItemPedido item) {
+    return ListTile(
+      dense: true,
+      title: Text(
+        item.nome,
+        maxLines: 1,
+        overflow: TextOverflow.ellipsis,
+      ),
+      leading: Icon(Icons.image),
+      subtitle: Text(
+        'Valor unitário: ${Carrinho.moeda} ${item.valor.toStringAsFixed(2).replaceAll('.', ',')}'
+        '\n'
+        'Quantidade: ${item.quantidade}',
+        maxLines: 2,
+      ),
+      trailing: Text(
+        '${Carrinho.moeda} ${item.subtotal.toStringAsFixed(2)}',
+        textAlign: TextAlign.center,
+      ),
+      onTap:
+
+          /// Apresenta a caixa de dialogo para edição do item.
+          () async {
+        await showDialog(
+          context: context,
+          useSafeArea: true,
+          barrierDismissible: false,
+          builder: (context) => DialogEditarItem(
+            carrinho: carrinho,
+            item: item,
+          ),
+        );
+      },
     );
   }
 }
