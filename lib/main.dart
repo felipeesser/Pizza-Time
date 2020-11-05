f06515f50d8afec16500b27bacf085a0103f2668
Felipe <felipeesser@id.uff.br>
2020-11-05 09:16:33 -0300

    unificacao temas

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,6 @@ import 'package:pizza_time/telas/home_files_rest/Home_Rest.dart';
 import 'package:provider/provider.dart';
 import 'telas/Login.dart';
 import 'package:pizza_time/modelo/Rotas.dart' as router;
-import 'package:google_fonts/google_fonts.dart';
 void main() => runApp(MultiProvider(
     providers: [ChangeNotifierProvider(
       builder: (context)=>ItemNotifier(),
@@ -24,7 +23,7 @@ class MyApp extends StatelessWidget {
           appBarTheme: AppBarTheme(color: Color.fromRGBO(204, 41, 0, 1) ),
         ),
         onGenerateRoute: router.generateRoute,
-        initialRoute: "/home",
+        initialRoute: "/rev_pedido_rest",
     );
   }
 }
