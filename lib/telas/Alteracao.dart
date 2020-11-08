64954729baf5fd90916592f9f29e02c6150be396
Felipe <felipeesser@id.uff.br>
2020-11-08 11:28:10 -0300

    adequacao tamanho e tema

--- lib/telas/Alteracao.dart
+++ lib/telas/Alteracao.dart
@@ -8,166 +8,153 @@ class Alteracao extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: false,
       appBar: AppBar(
-        backgroundColor: Color(0xFFbd1918),
         centerTitle: true,
         title: Text('Alteração de Cadastro'),
       ),
-      body: Column(
-        children: <Widget>[
-          Container(
-            alignment: Alignment.center,
-            child: Text(
-              'Nome atual',
-              style: TextStyle(
-                fontSize: 20.0,
-                color: Colors.white
-              ),
-            ),
-            padding: EdgeInsets.all(10.0),
-            margin: EdgeInsets.only(top:35, bottom: 30, left: 50, right: 50),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(25),
-              color: Color(0xFFdd6311),
-            ),
-          ),
-          Container(
-            height: 50,
-            width: 300,
-            child: TextFormField(
-              style: TextStyle(
-                fontSize: 20.0,
-                color: Colors.white,
-              ),
-              textAlign: TextAlign.center,
-              decoration: InputDecoration(
-                hintText: 'Novo nome',
-                hintStyle: TextStyle(
-                    fontSize: 20.0,
-                    color: Colors.white
-                ),
-                filled: true,
-                fillColor: Color(0xFFdd6311),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(25.0),
-                  ),
-                  borderSide: BorderSide(
-                    color: Colors.black,
-                    width: 1.0,
+      body: Container(
+        decoration: BoxDecoration(color: Color.fromRGBO(204, 41, 0, 1)),
+        padding: EdgeInsets.all(16),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.only(bottom: 8),
+                  child: TextField(
+                   // controller: _controllerNome,
+                    autofocus: true,
+                    keyboardType: TextInputType.text,
+                    style: TextStyle(fontSize: 20),
+                    decoration: InputDecoration(
+                        contentPadding: EdgeInsets.fromLTRB(32, 16, 32, 16),
+                        hintText: "Nome atual",
+                        filled: true,
+                        fillColor: Colors.white,
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(32))),
                   ),
                 ),
-              ),
-            ),
-          ),
-          Container(
-            alignment: Alignment.center,
-            child: Text(
-              'Senha atual',
-              style: TextStyle(
-                  fontSize: 20.0,
-                  color: Colors.white
-              ),
-            ),
-            padding: EdgeInsets.all(10.0),
-            margin: EdgeInsets.only(top: 25,bottom: 30, left: 50, right: 50),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(25),
-              color: Color(0xFFdd6311),
-            ),
-          ),
-          Container(
-            height: 50,
-            width: 300,
-            child: TextFormField(
-              style: TextStyle(
-                fontSize: 20.0,
-                color: Colors.white
-              ),
-              textAlign: TextAlign.center,
-              decoration: InputDecoration(
-                hintText: 'Nova senha',
-                hintStyle: TextStyle(
-                    fontSize: 20.0,
-                    color: Colors.white
-                ),
-                filled: true,
-                fillColor: Color(0xFFdd6311),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(25.0),
-                  ),
-                  borderSide: BorderSide(
-                    color: Colors.black,
-                    width: 1.0,
+                Padding(
+                  padding: EdgeInsets.only(bottom: 8),
+                  child: TextField(
+                   // controller: _controllerEmail,
+                    keyboardType: TextInputType.emailAddress,
+                    style: TextStyle(fontSize: 20),
+                    decoration: InputDecoration(
+                        contentPadding: EdgeInsets.fromLTRB(32, 16, 32, 16),
+                        hintText: "E-mail atual",
+                        filled: true,
+                        fillColor: Colors.white,
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(32))),
                   ),
                 ),
-              ),
-            ),
-          ),
-          Container(
-            alignment: Alignment.center,
-            child: Text(
-              'Endereço',
-              style: TextStyle(
-                  fontSize: 20.0,
-                  color: Colors.white
-              ),
-            ),
-            padding: EdgeInsets.all(10.0),
-            margin: EdgeInsets.only(top: 25,bottom: 30, left: 50, right: 50),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(25),
-              color: Color(0xFFdd6311),
-            ),
-          ),
-          Container(
-            height: 50,
-            width: 300,
-            child: TextFormField(
-              style: TextStyle(
-                fontSize: 20.0,
-                color: Colors.white
-              ),
-              textAlign: TextAlign.center,
-              decoration: InputDecoration(
-                hintText: 'Novo Endereço',
-                hintStyle: TextStyle(
-                    fontSize: 20.0,
-                    color: Colors.white
-                ),
-                filled: true,
-                fillColor: Color(0xFFdd6311),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(25.0),
-                  ),
-                  borderSide: BorderSide(
-                    color: Colors.black,
-                    width: 1.0,
+                Padding(
+                  padding: EdgeInsets.only(bottom: 8),
+                  child: TextField(
+                    //controller: _controllerSenha,
+                    obscureText: true,
+                    keyboardType: TextInputType.text,
+                    style: TextStyle(fontSize: 20),
+                    decoration: InputDecoration(
+                        contentPadding: EdgeInsets.fromLTRB(32, 16, 32, 16),
+                        hintText: "Senha atual",
+                        filled: true,
+                        fillColor: Colors.white,
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(32))),
                   ),
                 ),
-              ),
-            ),
-          ),
-          Container(
-            alignment: Alignment.center,
-            child: SizedBox(
-              width: 200,
-              height: 100,
-              child: RaisedButton(
-                child: Text(
-                  'Salvar',
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    color: Colors.white
+                Padding(
+                  padding: EdgeInsets.only(bottom: 8),
+                  child: TextField(
+                    //controller: _controllerEndereco,
+                    obscureText: true,
+                    keyboardType: TextInputType.text,
+                    style: TextStyle(fontSize: 20),
+                    decoration: InputDecoration(
+                        contentPadding: EdgeInsets.fromLTRB(32, 16, 32, 16),
+                        hintText: "Endereço atual",
+                        filled: true,
+                        fillColor: Colors.white,
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(32))),
                   ),
                 ),
-              ),
-            )
+                Row(
+                  children: <Widget>[
+                    Expanded(
+                      flex: 2,
+                      child: TextField(
+                       // controller: _controllerNumero,
+                        obscureText: true,
+                        keyboardType: TextInputType.text,
+                        //style: TextStyle(fontSize: 20),
+                        decoration: InputDecoration(
+                            contentPadding: EdgeInsets.fromLTRB(32, 16, 32, 16),
+                            hintText: "Nr atual",
+                            filled: true,
+                            fillColor: Colors.white,
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(32))),
+                      ),
+                    ),
+                    Expanded(
+                      flex: 4,
+                      child: Padding(
+                        padding: EdgeInsets.only(left: 4),
+                        child: TextField(
+                          //controller: _controllerComplemento,
+                          obscureText: true,
+                          keyboardType: TextInputType.text,
+                          style: TextStyle(fontSize: 20),
+                          decoration: InputDecoration(
+                              contentPadding:
+                              EdgeInsets.fromLTRB(32, 16, 32, 16),
+                              hintText: "Complemento atual",
+                              filled: true,
+                              fillColor: Colors.white,
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(32))),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 8),
+                  child: TextField(
+                    //controller: _controllerTelefone,
+                    obscureText: true,
+                    keyboardType: TextInputType.text,
+                    style: TextStyle(fontSize: 20),
+                    decoration: InputDecoration(
+                        contentPadding: EdgeInsets.fromLTRB(32, 16, 32, 16),
+                        hintText: "Telefone atual",
+                        filled: true,
+                        fillColor: Colors.white,
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(32))),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 16, bottom: 10),
+                  child: RaisedButton(
+                      child: Text(
+                        "Atualizar",
+                        style: TextStyle(color: Colors.black, fontSize: 20),
+                      ),
+                      color: Colors.white,
+                      onPressed: () {
+                       // _validarCampos();
+                      }),
+                ),
+              ],
+            ),
           ),
-        ],
+        ),
       ),
     );
   }
