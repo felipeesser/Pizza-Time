9e32f30e5b5beb296f06fd10b74bf3d5e872a9c8
Felipe <felipeesser@id.uff.br>
2020-11-10 11:32:41 -0300

    adequacao da apresentacao da tela para diferentes tamanhos

--- lib/telas/Detalhes.dart
+++ lib/telas/Detalhes.dart
@@ -17,25 +17,19 @@ class Detalhes extends StatelessWidget {
               children: <Widget>[
                 Align(
                   alignment: Alignment.topLeft,
-                  child: Container(
-                    padding: EdgeInsets.symmetric(horizontal: 15.0, vertical: 35.0),
-                    child: Text(
-                      'Endereço da Entrega',
-                      style: TextStyle(
-                        fontSize: 20.0
-                      ),
+                  child: Text(
+                    'Endereço da Entrega',
+                    style: TextStyle(
+                      fontSize: 20.0
                     ),
                   ),
                 ),
                 Align(
                   alignment: Alignment.topLeft,
-                  child: Container(
-                    padding: EdgeInsets.symmetric(horizontal: 15.0),
-                    child: Text(
-                      'Valor: 20,00\u0024',
-                      style: TextStyle(
-                        fontSize: 20.0
-                      ),
+                  child: Text(
+                    'Valor: 20,00\u0024',
+                    style: TextStyle(
+                      fontSize: 20.0
                     ),
                   ),
                 )
@@ -47,38 +41,25 @@ class Detalhes extends StatelessWidget {
             thickness: 1.0,
             color: Color(0xFF000000),
           ),
-          Container(
-            margin: EdgeInsets.all(40),
-            child: Text(
-              'Status do Pedido:',
-              style: TextStyle(
-                fontSize: 22.0
-              ),
+          Text(
+            'Status do Pedido:',
+            style: TextStyle(
+              fontSize: 22.0
             ),
           ),
-          Container(
-            margin: EdgeInsets.all(35),
-            child: Text(
-              'Preparando...',
-              style: TextStyle(
-                fontSize: 18
-              ),
+          Text(
+            'Preparando...',
+            style: TextStyle(
+              fontSize: 18
             ),
           ),
-          Container(
-            alignment: Alignment.bottomCenter,
-            margin: EdgeInsets.only(top:120.0, left: 20, right: 20),
-            padding: EdgeInsets.all(10.0),
+          FlatButton(
+            onPressed: (){},
             child: Text(
               'Contato com o restaurante',
               style: TextStyle(
               fontSize: 20.0,
-              color: Colors.white,
               ),
-            ),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(25),
-              color: Color(0xFFbd1918),
             ),
           )
         ]
--- lib/telas/home_files_rest/Abertura.dart
+++ lib/telas/home_files_rest/Abertura.dart
@@ -1,7 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:pizza_time/DiaSemana.dart';
+import 'package:pizza_time/modelo/DiaSemana.dart';
 
 class Abertura extends StatefulWidget {
   @override
@@ -27,7 +27,8 @@ class _AberturaState extends State<Abertura> {
     if(picked!=null && picked !=_timei){
       setState((){
         _timei=picked;
-        _dias.elementAt(_dias.indexOf(dropdownValue)).abertura=_timei;
+        if(dropdownValue!=null) _dias.elementAt(_dias.indexOf(dropdownValue)).abertura=_timei;
+        else _dias.elementAt(0).abertura=_timei;
       });
     }
   }
@@ -39,7 +40,8 @@ class _AberturaState extends State<Abertura> {
     if(picked!=null && picked !=_timef){
       setState((){
         _timef=picked;
-        _dias.elementAt(_dias.indexOf(dropdownValue)).fechamento=_timef;
+        if(dropdownValue!=null)_dias.elementAt(_dias.indexOf(dropdownValue)).fechamento=_timef;
+        else _dias.elementAt(0).fechamento=_timef;
       });
     }
   }
@@ -51,54 +53,67 @@ class _AberturaState extends State<Abertura> {
         title: Text('Horário de Funcionamento'),
         centerTitle: true,
       ),
-       body: Column(
-         children: [
-           Text("Horário de Funcionamento:"),
-           Table(
-             border: TableBorder.all(),
-             children: _dias.map((e) => TableRow(
-                 children: [Text(e.dia),
-                   Text('${e.abertura.hour}:${e.abertura.minute}'),
-                 Text('${e.fechamento.hour}:${e.fechamento.minute}')])).toList()
-           ),
-           Text('Editar Horário:'),
-           DropdownButton<DiaSemana>(
-             value: dropdownValue==null?_dias.elementAt(0):dropdownValue,
-             icon: Icon(Icons.arrow_drop_down),
-             onChanged: (DiaSemana newValue) {
-               setState(() {
-                 dropdownValue = newValue;
-               });
-             },
-             items: _dias.map((DiaSemana dia) {
-               return new DropdownMenuItem<DiaSemana>(
-                 value: dia,
-                 child: new Text(
-                   dia.dia,
+       body: Container(
+         padding: EdgeInsets.all(16),
+         child: Center(
+           child: SingleChildScrollView(
+             child: Column(
+               children: [
+                 Text("Horário de Funcionamento:"),
+                 Table(
+                   border: TableBorder.all(),
+                   children: _dias.map((e) => TableRow(
+                       children: [Text(e.dia),
+                         Text('${e.abertura.hour}:${e.abertura.minute}'),
+                       Text('${e.fechamento.hour}:${e.fechamento.minute}')])).toList()
                  ),
-               );
-             }).toList(),
+                 Text('Editar Horário:'),
+                 DropdownButton<DiaSemana>(
+                   value: dropdownValue==null?_dias.elementAt(0):dropdownValue,
+                   icon: Icon(Icons.arrow_drop_down),
+                   onChanged: (DiaSemana newValue) {
+                     setState(() {
+                       dropdownValue = newValue;
+                     });
+                   },
+                   items: _dias.map((DiaSemana dia) {
+                     return new DropdownMenuItem<DiaSemana>(
+                       value: dia,
+                       child: new Text(
+                         dia.dia,
+                       ),
+                     );
+                   }).toList(),
+                 ),
+                 Table(
+                   children: [
+                     TableRow(
+                       children: [
+                         Center(child: Text('Abertura:')),
+                         Center(child: Text('Fechamento:')),]),
+                         TableRow(
+                           children: [
+                             FlatButton(
+                               child: Text("${_timei.hour}:${_timei.minute}"),
+                               onPressed: (){
+                                 _selectTimei(context);
+                               },
+                             ),
+                             FlatButton(
+                               child: Text("${_timef.hour}:${_timef.minute}"),
+                               onPressed: (){
+                                 _selectTimef(context);
+                               },
+                             ),
+                           ],
+                         ),
+                   ],
+                 ),
+                 FlatButton(onPressed: (){}, child: Text("Atualizar"))
+               ],
+             ),
            ),
-           Row(
-             mainAxisAlignment: MainAxisAlignment.center,
-             children: [
-               Text('Abertura:'),
-               FlatButton(
-                 child: Text("${_timei.hour}:${_timei.minute}"),
-                 onPressed: (){
-                   _selectTimei(context);
-                 },
-               ),
-               Text('Fechamento:'),
-               FlatButton(
-                 child: Text("${_timef.hour}:${_timef.minute}"),
-                 onPressed: (){
-                   _selectTimef(context);
-                 },
-               ),
-             ],
-           ),
-         ],
+         ),
        ),
     );
   }
