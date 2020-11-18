9fe0d0a6dc91ed748eca522c1260ac23e2c93319
Gabriel Borba <50279580+gborba-uff-cc@users.noreply.github.com>
2020-11-18 19:04:14 -0300

    adicionados mais atributos, getter e setter

--- lib/modelo/Item.dart
+++ lib/modelo/Item.dart
@@ -1,10 +1,22 @@
-import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-class Item{
+class Item {
+  String _idItem;
   String nome;
+  String descricao;
+  double preco;
   String imagem;
-  Item.fromMap(Map<String, dynamic> data){
-    nome=data['nome'];
-    imagem=data['imagem'];
+  String tipo;
+
+  Item();
+
+  Item.fromMap(Map<String, dynamic> data) {
+    fromMap(data);
+  }
+
+  // ignore: unnecessary_getters_setters
+  String get idItem => _idItem;
+
+  // ignore: unnecessary_getters_setters
+  set idItem(String id) {
+    _idItem ??= id;
   }
 }
