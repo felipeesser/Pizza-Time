2832540cd83f828867600e6a1f96dba1ec57cccd
Gabriel Borba <50279580+gborba-uff-cc@users.noreply.github.com>
2020-11-21 23:09:33 -0300

    limpando o código

--- lib/notifier/CarrinhoNotifier.dart
+++ lib/notifier/CarrinhoNotifier.dart
@@ -1,26 +1,23 @@
-import 'dart:collection';
 import 'package:flutter/material.dart';
-import 'package:pizza_time/modelo/Item.dart';
+
 import 'package:pizza_time/modelo/carrinho.dart';
 import 'package:pizza_time/modelo/item_carrinho.dart';
+
 class CarrinhoNotifier extends ChangeNotifier{
-  //List<ItemCarrinho> _listaItens =[];
   Carrinho _carrinhoAtual=Carrinho();
-  //UnmodifiableListView<Item> get listaItens=>UnmodifiableListView(_listaItens);
   Carrinho get carrinhoAtual=>_carrinhoAtual;
-//  set listaItens(List<Item> value) {
-//    _listaItens = value;
-//    notifyListeners();
-//  }
+
   set carrinhoAtual(Carrinho value) {
     _carrinhoAtual = value;
     notifyListeners();
   }
-  adicionarItem(ItemCarrinho item){
+
+  void adicionarItem(ItemCarrinho item){
     _carrinhoAtual.itensCarrinho.add(item);
     notifyListeners();
   }
-  removerItem(ItemCarrinho item){
+
+  void removerItem(ItemCarrinho item){
     _carrinhoAtual.itensCarrinho.remove(item);
     notifyListeners();
   }
