6d496dd26058edcf6bed6f9b888641c10d8f84e9
Gabriel Borba <50279580+gborba-uff-cc@users.noreply.github.com>
2020-11-22 15:45:22 -0300

    adicionadas as funções de update e remoção que estavam faltando

--- lib/api/usuario_firestore.dart
+++ lib/api/usuario_firestore.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+
 import 'package:pizza_time/modelo/Usuario.dart';
 
 const pathUsuarios = '/usuarios';
@@ -13,4 +14,35 @@ void create(Usuario usuario) async {
 Future<Usuario> read(DocumentReference documento) async {
   final snapshot = await documento?.get();
   return snapshot.data == null ? null : Usuario.fromMap(snapshot.data);
+}
+
+/// Atualiza o [usuario] no firestore, com o valor atual do [usuario].
+///
+/// ```dart
+/// ...
+/// await update(usuario);
+/// ...
+/// ```
+void updade(Usuario usuario) async {
+  if (usuario == null) {
+    return;
+  }
+  DocumentReference documento =
+      Firestore.instance.collection(pathUsuarios).document(usuario.idUsuario);
+  await documento.updateData(usuario.toMap());
+}
+
+/// Remove um usuario com [idUsuario] da coleção de usuarios.
+///
+/// Essa função não remove o cadastro do usuario da autenticação no FirestoreAuth.
+///
+/// ```dart
+/// ...
+/// await delete(usuario.idUsuario);
+/// ...
+/// ```
+Future<void> delete(String idUsuario) async {
+  DocumentReference documento =
+      Firestore.instance.collection(pathUsuarios).document(idUsuario);
+  documento.delete();
 }
