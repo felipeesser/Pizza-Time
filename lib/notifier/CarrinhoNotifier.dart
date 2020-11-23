292dc0e5b45b9b71291d4c582e84a60a91593b74
Gabriel Borba <50279580+gborba-uff-cc@users.noreply.github.com>
2020-11-23 13:07:33 -0300

    adicionada função para esvaziar o carrinho de compras.

--- lib/notifier/CarrinhoNotifier.dart
+++ lib/notifier/CarrinhoNotifier.dart
@@ -3,22 +3,28 @@ import 'package:flutter/material.dart';
 import 'package:pizza_time/modelo/carrinho.dart';
 import 'package:pizza_time/modelo/item_carrinho.dart';
 
-class CarrinhoNotifier extends ChangeNotifier{
-  Carrinho _carrinhoAtual=Carrinho();
-  Carrinho get carrinhoAtual=>_carrinhoAtual;
+class CarrinhoNotifier extends ChangeNotifier {
+  Carrinho _carrinhoAtual = Carrinho();
+  Carrinho get carrinhoAtual => _carrinhoAtual;
 
   set carrinhoAtual(Carrinho value) {
     _carrinhoAtual = value;
     notifyListeners();
   }
 
-  void adicionarItem(ItemCarrinho item){
+  void adicionarItem(ItemCarrinho item) {
     _carrinhoAtual.itensCarrinho.add(item);
     notifyListeners();
   }
 
-  void removerItem(ItemCarrinho item){
+  void removerItem(ItemCarrinho item) {
     _carrinhoAtual.itensCarrinho.remove(item);
+    notifyListeners();
+  }
+
+  // Remove todos os itens do carrinho.
+  void esvaziarCarrinho() {
+    _carrinhoAtual.itensCarrinho.clear();
     notifyListeners();
   }
 }
