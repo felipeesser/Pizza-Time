ee5ce509c34b7887bd005fd5b54225ab7c454967
brandao <brandvictor09@gmail.com>
2020-11-24 16:24:20 -0300

    funcionamento

--- lib/telas/home_files_rest/Abertura.dart
+++ lib/telas/home_files_rest/Abertura.dart
@@ -2,6 +2,9 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:pizza_time/modelo/DiaSemana.dart';
+import 'package:pizza_time/api/funcionamento_firebase.dart'
+    as funcionamentoFirebaseCrud;
+import 'package:pizza_time/modelo/funcionamento.dart';
 
 class Abertura extends StatefulWidget {
   static final nomeTela = "/abertura";
@@ -10,39 +13,42 @@ class Abertura extends StatefulWidget {
 }
 
 class _AberturaState extends State<Abertura> {
-  List<DiaSemana> _dias=[DiaSemana("Segunda",TimeOfDay.now(),TimeOfDay.now()),
-    DiaSemana("Terça",TimeOfDay.now(),TimeOfDay.now()),
-    DiaSemana("Quarta",TimeOfDay.now(),TimeOfDay.now()),
-    DiaSemana("Quinta",TimeOfDay.now(),TimeOfDay.now()),
-    DiaSemana("Sexta",TimeOfDay.now(),TimeOfDay.now()),
-    DiaSemana("Sábado",TimeOfDay.now(),TimeOfDay.now()),
-    DiaSemana("Domingo",TimeOfDay.now(),TimeOfDay.now()),];
-  DiaSemana dropdownValue=null;
-  TimeOfDay _timei= new TimeOfDay.now();
-  TimeOfDay _timef= new TimeOfDay.now();
-  Future<Null> _selectTimei(BuildContext context) async{
-    final TimeOfDay picked= await showTimePicker(
-      context: context,
-      initialTime: _timei
-    );
-    if(picked!=null && picked !=_timei){
-      setState((){
-        _timei=picked;
-        if(dropdownValue!=null) _dias.elementAt(_dias.indexOf(dropdownValue)).abertura=_timei;
-        else _dias.elementAt(0).abertura=_timei;
+  List<DiaSemana> _dias = [
+    DiaSemana("Segunda", TimeOfDay.now(), TimeOfDay.now()),
+    DiaSemana("Terça", TimeOfDay.now(), TimeOfDay.now()),
+    DiaSemana("Quarta", TimeOfDay.now(), TimeOfDay.now()),
+    DiaSemana("Quinta", TimeOfDay.now(), TimeOfDay.now()),
+    DiaSemana("Sexta", TimeOfDay.now(), TimeOfDay.now()),
+    DiaSemana("Sábado", TimeOfDay.now(), TimeOfDay.now()),
+    DiaSemana("Domingo", TimeOfDay.now(), TimeOfDay.now()),
+  ];
+  DiaSemana dropdownValue = null;
+  TimeOfDay _timei = new TimeOfDay.now();
+  TimeOfDay _timef = new TimeOfDay.now();
+  Future<Null> _selectTimei(BuildContext context) async {
+    final TimeOfDay picked =
+        await showTimePicker(context: context, initialTime: _timei);
+    if (picked != null && picked != _timei) {
+      setState(() {
+        _timei = picked;
+        if (dropdownValue != null)
+          _dias.elementAt(_dias.indexOf(dropdownValue)).abertura = _timei;
+        else
+          _dias.elementAt(0).abertura = _timei;
       });
     }
   }
-  Future<Null> _selectTimef(BuildContext context) async{
-    final TimeOfDay picked= await showTimePicker(
-        context: context,
-        initialTime: _timef
-    );
-    if(picked!=null && picked !=_timef){
-      setState((){
-        _timef=picked;
-        if(dropdownValue!=null)_dias.elementAt(_dias.indexOf(dropdownValue)).fechamento=_timef;
-        else _dias.elementAt(0).fechamento=_timef;
+
+  Future<Null> _selectTimef(BuildContext context) async {
+    final TimeOfDay picked =
+        await showTimePicker(context: context, initialTime: _timef);
+    if (picked != null && picked != _timef) {
+      setState(() {
+        _timef = picked;
+        if (dropdownValue != null)
+          _dias.elementAt(_dias.indexOf(dropdownValue)).fechamento = _timef;
+        else
+          _dias.elementAt(0).fechamento = _timef;
       });
     }
   }
@@ -54,68 +60,111 @@ class _AberturaState extends State<Abertura> {
         title: Text('Horário de Funcionamento'),
         centerTitle: true,
       ),
-       body: Container(
-         padding: EdgeInsets.all(16),
-         child: Center(
-           child: SingleChildScrollView(
-             child: Column(
-               children: [
-                 Text("Horário de Funcionamento:"),
-                 Table(
-                   border: TableBorder.all(),
-                   children: _dias.map((e) => TableRow(
-                       children: [Text(e.dia),
-                         Text('${e.abertura.hour}:${e.abertura.minute}'),
-                       Text('${e.fechamento.hour}:${e.fechamento.minute}')])).toList()
-                 ),
-                 Text('Editar Horário:'),
-                 DropdownButton<DiaSemana>(
-                   value: dropdownValue==null?_dias.elementAt(0):dropdownValue,
-                   icon: Icon(Icons.arrow_drop_down),
-                   onChanged: (DiaSemana newValue) {
-                     setState(() {
-                       dropdownValue = newValue;
-                     });
-                   },
-                   items: _dias.map((DiaSemana dia) {
-                     return new DropdownMenuItem<DiaSemana>(
-                       value: dia,
-                       child: new Text(
-                         dia.dia,
-                       ),
-                     );
-                   }).toList(),
-                 ),
-                 Table(
-                   children: [
-                     TableRow(
-                       children: [
-                         Center(child: Text('Abertura:')),
-                         Center(child: Text('Fechamento:')),]),
-                         TableRow(
-                           children: [
-                             FlatButton(
-                               child: Text("${_timei.hour}:${_timei.minute}"),
-                               onPressed: (){
-                                 _selectTimei(context);
-                               },
-                             ),
-                             FlatButton(
-                               child: Text("${_timef.hour}:${_timef.minute}"),
-                               onPressed: (){
-                                 _selectTimef(context);
-                               },
-                             ),
-                           ],
-                         ),
-                   ],
-                 ),
-                 FlatButton(onPressed: (){}, child: Text("Atualizar"))
-               ],
-             ),
-           ),
-         ),
-       ),
+      body: Container(
+        padding: EdgeInsets.all(16),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Text("Horário de Funcionamento:"),
+                Table(
+                    border: TableBorder.all(),
+                    children: _dias
+                        .map((e) => TableRow(children: [
+                              Text(e.dia),
+                              Text('${e.abertura.hour}:${e.abertura.minute}'),
+                              Text(
+                                  '${e.fechamento.hour}:${e.fechamento.minute}')
+                            ]))
+                        .toList()),
+                Text('Editar Horário:'),
+                DropdownButton<DiaSemana>(
+                  value: dropdownValue == null
+                      ? _dias.elementAt(0)
+                      : dropdownValue,
+                  icon: Icon(Icons.arrow_drop_down),
+                  onChanged: (DiaSemana newValue) {
+                    setState(() {
+                      dropdownValue = newValue;
+                    });
+                  },
+                  items: _dias.map((DiaSemana dia) {
+                    return new DropdownMenuItem<DiaSemana>(
+                      value: dia,
+                      child: new Text(
+                        dia.dia,
+                      ),
+                    );
+                  }).toList(),
+                ),
+                Table(
+                  children: [
+                    TableRow(children: [
+                      Center(child: Text('Abertura:')),
+                      Center(child: Text('Fechamento:')),
+                    ]),
+                    TableRow(
+                      children: [
+                        FlatButton(
+                          child: Text("${_timei.hour}:${_timei.minute}"),
+                          onPressed: () {
+                            _selectTimei(context);
+                          },
+                        ),
+                        FlatButton(
+                          child: Text("${_timef.hour}:${_timef.minute}"),
+                          onPressed: () {
+                            _selectTimef(context);
+                          },
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                FlatButton(
+                    onPressed: () async {
+                      var horario = Funcionamento.fromMap({
+                        '1': {
+                          'abertura': '${_dias.elementAt(0).abertura}',
+                          'fechamento': '${_dias.elementAt(0).fechamento}'
+                        },
+                        '2': {
+                          'abertura': '${_dias.elementAt(1).abertura}',
+                          'fechamento': '${_dias.elementAt(1).fechamento}'
+                        },
+                        '3': {
+                          'abertura': '${_dias.elementAt(2).abertura}',
+                          'fechamento': '${_dias.elementAt(2).fechamento}'
+                        },
+                        '4': {
+                          'abertura': '${_dias.elementAt(3).abertura}',
+                          'fechamento': '${_dias.elementAt(3).fechamento}'
+                        },
+                        '5': {
+                          'abertura': '${_dias.elementAt(4).abertura}',
+                          'fechamento': '${_dias.elementAt(4).fechamento}'
+                        },
+                        '6': {
+                          'abertura': '${_dias.elementAt(5).abertura}',
+                          'fechamento': '${_dias.elementAt(5).fechamento}'
+                        },
+                        '7': {
+                          'abertura': '${_dias.elementAt(6).abertura}',
+                          'fechamento': '${_dias.elementAt(6).fechamento}'
+                        }
+                      });
+                      if (await funcionamentoFirebaseCrud.read() == null) {
+                        funcionamentoFirebaseCrud.create(horario);
+                      } else {
+                        funcionamentoFirebaseCrud.update(horario);
+                      }
+                    },
+                    child: Text("Atualizar"))
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
